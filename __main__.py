8974dc5de729aeec2ba04c58cd82755848ede1d0
Alejandro Minor <aminorj@iingen.unam.mx>
2024-05-01 21:31:30 -0600

    [ADD] Funciones basicas para el juego de trivia

---
+++ __main__.py
@@ -0,0 +1,6 @@
+import elixir_trivia
+
+if __name__ == '__main__':
+    trivia = elixir_trivia.ElixirTrivia()
+    trivia.launch_trivia()
+    trivia.play_again()
---
+++ elixir_trivia.py
@@ -0,0 +1,55 @@
+import requests
+import random
+
+class ElixirTrivia:
+    def __init__(self):
+        self.url = 'https://api.clashroyale.com/v1/cards'
+        self.headers = {
+            'Authorization': 'Bearer APIKEY'
+        }
+        self.cards = self.get_cards()
+
+    def get_cards(self):
+        response = requests.get(self.url, headers=self.headers)
+        if response.status_code == 200:
+            cards = response.json()
+            return cards
+        else:
+            return None
+    
+    def get_random_card(self):
+        card = random.choice(self.cards['items'])
+        return card
+
+    def get_card_elixir_trivia(self):
+        card = self.get_random_card()
+        card_name = card['name']
+        card_elixir_cost = card['elixirCost']
+        return card_name, card_elixir_cost
+
+
+    def launch_trivia(self):
+        card_name, card_elixir_cost = self.get_card_elixir_trivia()
+        print(f'Card Name: {card_name}')
+        while True:
+            user_input = input('Guess the elixir cost: ')
+            if user_input == str(card_elixir_cost):
+                print('Correct!')
+                break
+            else:
+                print('Incorrect! Try again.')
+
+    def play_again(self):
+        while True:
+            user_input = input('Do you want to play again? (y/n): ')
+            if user_input.lower() == 'y':
+                self.launch_trivia()
+            elif user_input.lower() == 'n':
+                print('Thanks for playing!')
+                break
+            else:
+                print('Invalid input! Try again.')
+
+
+
+    
